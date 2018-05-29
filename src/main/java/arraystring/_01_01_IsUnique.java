bc9f571194acdca49a3a7a03dcc80b0521219ebc
lyv <vy.ly@autodesk.com>
2018-05-29 13:59:41 -0700

    1.1 isUnique

--- src/main/java/arraystring/_01_01_IsUnique.java
+++ src/main/java/arraystring/_01_01_IsUnique.java
@@ -7,7 +7,16 @@ package arraystring;
 class _01_01_IsUnique {
 
     boolean isUnique(String str) {
-        throw new UnsupportedOperationException();
+        boolean [] letters = new boolean[256];
+        for (int i = 0; i < str.length(); i++){
+            char c = str.charAt(i);
+            if(letters[c-'a']){
+              return false;
+            } else{
+              letters[c-'a'] = true;
+            }
+        }
+        return true;
     }
 
 }
