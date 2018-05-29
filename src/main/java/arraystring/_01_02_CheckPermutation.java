2cb1ecd426e2c23af0a2d5230b4c0ec25635e9e4
lyv <vy.ly@autodesk.com>
2018-05-29 14:46:48 -0700

    isUn

--- src/main/java/arraystring/_01_02_CheckPermutation.java
+++ src/main/java/arraystring/_01_02_CheckPermutation.java
@@ -1,10 +1,21 @@
 package arraystring;
 
+import java.util.Arrays;
+import java.util.Collections;
+
 /**
  * Given two strings,write a method to decide if one is a permutation of the other.
  */
 class _01_02_CheckPermutation {
     boolean check(String a, String b) {
-        throw new UnsupportedOperationException();
+        char[] tempa = a.toCharArray();
+        char[] tempb = b.toCharArray();
+
+        Arrays.sort(tempa);
+        Arrays.sort(tempb);
+        String sorteda = new String(tempa);
+        String sortedb = new String(tempb);
+        return sorteda.equals(sortedb);
+
     }
 }
