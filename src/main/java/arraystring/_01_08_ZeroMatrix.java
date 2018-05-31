88fd9636540f9ebcd163433dd8e1d613f56dfe7e
lyv <vy.ly@autodesk.com>
2018-05-31 16:19:15 -0700

    1.8 zeromatrix

--- src/main/java/arraystring/_01_08_ZeroMatrix.java
+++ src/main/java/arraystring/_01_08_ZeroMatrix.java
@@ -5,6 +5,23 @@ package arraystring;
  */
 class _01_08_ZeroMatrix {
     int[][] zero(int[][] matrix) {
-        throw new UnsupportedOperationException();
+        boolean[] rowZero = new boolean[matrix.length];
+        boolean[] colZero = new boolean[matrix[0].length];
+        for(int i = 0; i < matrix.length; i++) {
+            for ( int j = 0; j < matrix[0].length; j++) {
+                if(matrix[i][j] == 0) {
+                    rowZero[i] = true;
+                    colZero[j] = true;
+                }
+            }
+        }
+        for(int i = 0; i < matrix.length; i++) {
+            for ( int j = 0; j < matrix[0].length; j++) {
+                if(rowZero[i] || colZero[j]) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        return matrix;
     }
 }
