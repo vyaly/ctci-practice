c382736f9281275d99816cd28f367ac3e3cc252c
lyv <vy.ly@autodesk.com>
2018-05-31 10:00:46 -0700

    2.5 reverse

--- src/main/java/linkedlist/_02_05_SumListReverse.java
+++ src/main/java/linkedlist/_02_05_SumListReverse.java
@@ -12,6 +12,24 @@ package linkedlist;
  */
 class _02_05_SumListReverse {
     LinkedListNode sum(LinkedListNode l1, LinkedListNode l2) {
-        throw new UnsupportedOperationException();
+        if(l1 == null && l2 == null) return null;
+        int carry = 0;
+        LinkedListNode dummy = new LinkedListNode(-1);
+        LinkedListNode tail = dummy;
+        while (l1 != null || l2 != null || carry > 0){
+            int sum = carry;
+            if(l1 != null) {
+                sum += l1.val;
+                l1 = l1.next;
+            }
+            if(l2 != null ) {
+                sum += l2.val;
+                l2 = l2.next;
+            }
+            tail.next = new LinkedListNode(sum%10);
+            tail = tail.next;
+            carry = sum/10;
+        }
+        return dummy.next;
     }
 }
