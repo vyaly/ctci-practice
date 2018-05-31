2349ae87c6ecc1bfcfae30cc76858ac5b61f3e69
lyv <vy.ly@autodesk.com>
2018-05-30 18:33:51 -0700

    2.2 returnkthtolast

--- src/main/java/linkedlist/_02_02_ReturnKthToLast.java
+++ src/main/java/linkedlist/_02_02_ReturnKthToLast.java
@@ -6,6 +6,21 @@ package linkedlist;
 class _02_02_ReturnKthToLast {
 
     LinkedListNode kthToLast(LinkedListNode head, int k) {
-        throw new UnsupportedOperationException();
+        LinkedListNode runner = head;
+        LinkedListNode curr = head;
+        for(int i = 0; i < k; i++) {
+            if(runner == null) return null;
+            runner = runner.next;
+        }
+
+        if(runner == null) return null;
+
+
+        while(runner.next!= null){
+            runner = runner.next;
+            curr = curr.next;
+        }
+
+        return curr;
     }
 }
