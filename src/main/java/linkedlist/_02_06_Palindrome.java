419fd80e44f417e53b8aafec6bc0d5d9886f1ed0
lyv <vy.ly@autodesk.com>
2018-05-31 17:02:57 -0700

    palindrome

--- src/main/java/linkedlist/_02_06_Palindrome.java
+++ src/main/java/linkedlist/_02_06_Palindrome.java
@@ -6,7 +6,27 @@ package linkedlist;
 class _02_06_Palindrome {
 
     boolean isPalindrome(LinkedListNode head) {
-        throw new UnsupportedOperationException();
+        if(head == null) return true;
+        LinkedListNode turtle = head;
+        LinkedListNode runner = head;
+        while(runner!= null && runner.next != null) {
+            runner = runner.next.next;
+            turtle = turtle.next;
+        }
+        if(runner == turtle) return true;
+        if(runner == null){
+            runner = head;
+            turtle = turtle.next;
+        } else {
+            turtle = turtle.next.next;
+            runner = head;
+        }
+        while(turtle != null){
+            if(runner.val != turtle.val) return false;
+            turtle = turtle.next;
+            runner = runner.next;
+        }
+        return true;
     }
 
 
