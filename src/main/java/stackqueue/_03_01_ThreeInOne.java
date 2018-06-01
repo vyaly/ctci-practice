743ed3d66d97d5fbac6882ca667fb922c12f8450
lyv <vy.ly@autodesk.com>
2018-06-01 10:55:46 -0700

    3.1 but can optimize to allow flexible array

--- src/main/java/stackqueue/_03_01_ThreeInOne.java
+++ src/main/java/stackqueue/_03_01_ThreeInOne.java
@@ -1,21 +1,42 @@
 package stackqueue;
 
+import java.util.Arrays;
+
 /**
  * Describe how you could use a single array to implement three stacks
  */
 class _03_01_ThreeInOne {
-
+    private int[] stacks;
+    private int[] pointers;
+    private int capacity;
 
     _03_01_ThreeInOne(int initSize) {
+        stacks = new int[initSize*3];
+        pointers = new int[]{-1, -1, -1};
+        capacity = initSize;
+        System.out.println(" initSize" + initSize);
 
     }
 
     void push(int stackNum, int val) {
-        throw new UnsupportedOperationException();
+        System.out.println(" Push " + stackNum + " val" + val);
+        if(stackNum < 0 || stackNum > 3) throw new IllegalArgumentException("stacknum is invalid");
+        pointers[stackNum] += 1;
+        if(pointers[stackNum] == capacity) throw new IllegalArgumentException("Stack " + stackNum + "is full");
+        int index = getHead(stackNum);
+        stacks[index] = val;
+        System.out.println(Arrays.toString(stacks) + " p" + Arrays.toString(pointers));
+//        throw new UnsupportedOperationException();
+    }
+    private int getHead( int stackNum){
+        return (int) capacity*stackNum + pointers[stackNum];
     }
 
     int pop(int stackNum) {
-        throw new UnsupportedOperationException();
+        int index = getHead(stackNum);
+        int val = stacks[index];
+        pointers[stackNum] -= 1;
+        return val;
     }
 
 
