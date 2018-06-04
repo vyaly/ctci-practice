83e128f86538ebf28f1cf73bdf7a2a0b7c99a6cb
lyv <vy.ly@autodesk.com>
2018-06-04 16:47:43 -0700

    triple step

--- src/main/java/recursivedp/_08_01_TripleStep.java
+++ src/main/java/recursivedp/_08_01_TripleStep.java
@@ -7,6 +7,19 @@ package recursivedp;
 class _08_01_TripleStep {
 
     int countWays(int steps) {
-        throw new UnsupportedOperationException();
+        int[] dp = new int[Math.max(4,steps + 1)];
+        dp[0] = 1;
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 4;
+        if(steps < 4) {
+            return dp[steps];
+        }
+
+        for(int i = 4; i <= steps; i++) {
+
+            dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
+        }
+        return dp[steps];
     }
 }
