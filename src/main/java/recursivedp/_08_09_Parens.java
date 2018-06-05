725cf2dd423e67761384234150d009df90912c49
lyv <vy.ly@autodesk.com>
2018-06-04 17:27:00 -0700

    parenthesis

--- src/main/java/recursivedp/_08_09_Parens.java
+++ src/main/java/recursivedp/_08_09_Parens.java
@@ -1,5 +1,6 @@
 package recursivedp;
 
+import java.util.HashSet;
 import java.util.Set;
 
 /**
@@ -9,9 +10,20 @@ import java.util.Set;
  * Output: ((())), (()()), (())(), ()(()), ()()()
  */
 public class _08_09_Parens {
+    Set<String> allPairs;
 
     Set<String> combine(int pairs) {
-        throw new UnsupportedOperationException();
+
+        allPairs = new HashSet<>();
+        if(pairs == 0) return allPairs;
+        helper(pairs,pairs,"");
+            return allPairs;
+    }
+
+    private void helper(int left, int right, String p) {
+        if(left == 0 && right == 0) allPairs.add(p);
+        if(left < right) helper(left,right-1, p + ")");
+        if(left > 0) helper(left-1,right, p + "(");
     }
 
 }
