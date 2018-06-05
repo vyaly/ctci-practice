fd87b54576dca3a82e2b1c5916b76d5900199cd3
lyv <vy.ly@autodesk.com>
2018-06-04 17:01:01 -0700

    triple step optimal solution with runtime O(n) and space O(1)

--- src/main/java/recursivedp/_08_01_TripleStep.java
+++ src/main/java/recursivedp/_08_01_TripleStep.java
@@ -5,7 +5,7 @@ package recursivedp;
  * Implement a method to count how many possible ways the child can run up the stairs.
  */
 class _08_01_TripleStep {
-
+/* Suboptimal because requires O(n) space
     int countWays(int steps) {
         int[] dp = new int[Math.max(4,steps + 1)];
         dp[0] = 1;
@@ -21,5 +21,27 @@ class _08_01_TripleStep {
             dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
         }
         return dp[steps];
+    }
+    */
+
+    /** optimal because uses O(1) space and run time is O(n)
+     *
+     * @param steps
+     * @return
+     */
+    int countWays(int steps) {
+        if(steps == 0) return 1;
+        else if(steps == 1) return 1;
+        else if(steps == 2) return 2;
+        else if(steps == 3) return 4;
+        int[] dp = new int[]{1,2,4};
+
+        for(int i = 4; i <= steps; i++) {
+            int temp = dp[2];
+            dp[2]  +=  dp[1] + dp[0];
+            dp[0] = dp[1];
+            dp[1] = temp;
+        }
+        return dp[2];
     }
 }
